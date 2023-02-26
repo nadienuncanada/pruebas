99df1b5544e802652b7b1668e0d34371b5ba5e60
Ignacio <meloignacionicolas@gmail.com>
2023-02-26 02:41:31 +0000

    Subi lo primero de go

---
+++ GoLang/main.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+type estruk struct{
+	nom string
+	cant int
+}
+var array [10] int
+func main(){
+	var a= "Texto auto definido?"
+	fmt.Println("Tipo de dato ",a)
+	for i:=1;i>10;i++{
+		array[i]=i
+	}
+	var b=estruk {nom:"pepe",cant:15}
+	fmt.Println(b)
+	fmt.Println(array)
+}
